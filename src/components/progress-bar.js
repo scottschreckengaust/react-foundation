69cc05b9eab32e8ec46f4024ce74dbfb198d3b6b
Christoffer Niska <christofferniska@gmail.com>
2016-03-12 22:31:59 +0200

    Add Progress component

---
+++ src/components/progress-bar.js
@@ -0,0 +1,114 @@
+import React from 'react';
+import check from 'check-types';
+import classNames from 'classnames';
+import { default as omit } from 'lodash.omit';
+
+export const ProgressColors = {
+  SUCCESS: 'success',
+  WARNING: 'warning',
+  ALERT: 'alert'
+};
+
+const omitProps = [
+  'color',
+  'minValue',
+  'maxValue',
+  'currentValue',
+  'valueText',
+  'meter'
+];
+
+/**
+ * ProgressBar class.
+ *
+ * @param {Object} props
+ * @returns {XML}
+ */
+export const Progress = props => {
+  check.assert.maybe.number(props.minValue, 'ProgressBar.props.minValue must be a number.');
+  check.assert.maybe.number(props.maxValue, 'ProgressBar.props.maxValue must be a number.');
+  check.assert.maybe.number(props.currentValue, 'ProgressBar.props.currentValue must be a number.');
+  check.assert.maybe.string(props.color, 'ProgressBar.props.color must be a string.');
+
+  const {
+    minValue = 0,
+    maxValue = 100,
+    currentValue = 0,
+    valueText,
+    meter: meterProps = {}
+  } = props;
+
+  const className = classNames(
+    props.className || 'progress',
+    props.color
+  );
+
+  return (
+    <div {...omit(props, omitProps)}
+      className={className}
+      role="progressbar"
+      aria-valuemin={minValue}
+      aria-valuemax={maxValue}
+      aria-valuenow={currentValue}
+      aria-valuetext={valueText}>
+      {meterProps.text ? <ProgressMeterWithText {...meterProps}/> : <ProgressMeter {...meterProps}/>}
+    </div>
+  );
+};
+
+/**
+ * ProgressMeter class.
+ *
+ * @param {Object} props
+ * @returns {XML}
+ */
+export const ProgressMeter = props => (
+  <div {...props} className={props.className || 'progress-meter'}></div>
+);
+
+/**
+ * ProgressMeterWithText class.
+ *
+ * @param {Object} props
+ * @returns {XML}
+ */
+export const ProgressMeterWithText = props => (
+  <span {...props} className={props.className || 'progress-meter'}>
+    <ProgressMeterText>{props.text}</ProgressMeterText>
+  </span>
+);
+
+/**
+ * ProgressMeterText class.
+ *
+ * @param {Object} props
+ * @returns {XML}
+ */
+export const ProgressMeterText = props => (
+  <p {...props} className={props.className || 'progress-meter-text'}/>
+);
+
+/**
+ * ProgressMeterText class.
+ *
+ * @param {Object} props
+ * @returns {XML}
+ */
+export const NativeProgress = props => {
+  check.assert.number(props.maxValue, 'ProgressBar.props.maxValue must be a number.');
+  check.assert.number(props.currentValue, 'ProgressBar.props.currentValue must be a number.');
+  check.assert.maybe.string(props.color, 'ProgressBar.props.color must be a string.');
+
+  const {
+    maxValue = 100,
+    currentValue = 0,
+  } = props;
+
+  return (
+    <progress className={classNames(props.className, props.color)} max={maxValue} value={currentValue}/>
+  );
+};
+
+// TODO: Add support for native meter
+
+export default Progress;
---
+++ test/components/progress-bar-spec.js
@@ -0,0 +1,160 @@
+import React from 'react';
+import { createRenderer } from 'react-addons-test-utils';
+import { render } from 'enzyme';
+import { expect } from 'chai';
+import Progress, {
+  ProgressMeter,
+  ProgressMeterWithText,
+  ProgressMeterText,
+  NativeProgress,
+  ProgressColors
+} from 'components/progress-bar';
+
+describe('Progress component', () => {
+
+  it('sets tag name', () => {
+    const component = render(<Progress/>);
+    expect(component).to.have.tagName('div');
+  });
+
+  it('sets class name', () => {
+    const component = render(<Progress/>);
+    expect(component).to.have.className('progress');
+  });
+
+  it('sets custom class name', () => {
+    const component = render(<Progress className="my-progress"/>);
+    expect(component).to.have.className('my-progress');
+    expect(component).to.not.have.className('progress');
+  });
+
+  it('sets role', () => {
+    const component = render(<Progress/>);
+    expect(component).to.have.attr('role', 'progressbar');
+  });
+
+  it('sets color', () => {
+    const component = render(<Progress color={ProgressColors.SUCCESS}/>);
+    expect(component).to.have.className('success');
+    expect(component).to.not.have.attr('color');
+  });
+
+  it('sets minimum value', () => {
+    const component = render(<Progress minValue={0}/>);
+    expect(component).to.have.attr('aria-valuemin', '0');
+  });
+
+  it('sets maximum value', () => {
+    const component = render(<Progress maxValue={100}/>);
+    expect(component).to.have.attr('aria-valuemax', '100');
+  });
+
+  it('sets current value', () => {
+    const component = render(<Progress currentValue={50}/>);
+    expect(component).to.have.attr('aria-valuenow', '50');
+  });
+
+  it('sets value text', () => {
+    const component = render(<Progress valueText="50 percent"/>);
+    expect(component).to.have.attr('aria-valuetext', '50 percent');
+  });
+
+  it('sets contents', () => {
+    const renderer = createRenderer();
+    renderer.render(<Progress/>);
+    const output = renderer.getRenderOutput();
+    expect(output).jsx.to.include(<ProgressMeter/>);
+  });
+
+});
+
+describe('ProgressMeter component', () => {
+
+  it('sets tag name', () => {
+    const component = render(<ProgressMeter/>);
+    expect(component).to.have.tagName('div');
+  });
+
+  it('sets class name', () => {
+    const component = render(<ProgressMeter/>);
+    expect(component).to.have.className('progress-meter');
+  });
+
+  it('sets custom class name', () => {
+    const component = render(<ProgressMeter className="my-progress-meter"/>);
+    expect(component).to.have.className('my-progress-meter');
+    expect(component).to.not.have.className('progress-meter');
+  });
+
+  it('sets width', () => {
+    const component = render(<ProgressMeter widthPercent={75}/>);
+    expect(component).to.have.style({ width: '75%' });
+  });
+
+});
+
+describe('ProgressMeterWithText component', () => {
+
+  it('sets text', () => {
+    const renderer = createRenderer();
+    renderer.render(<ProgressMeterWithText text="25%"/>);
+    const output = renderer.getRenderOutput();
+    expect(output).jsx.to.include(<ProgressMeterText>25%</ProgressMeterText>);
+  });
+
+});
+
+describe('ProgressMeterText component', () => {
+
+  it('sets tag name', () => {
+    const component = render(<ProgressMeterText/>);
+    expect(component).to.have.tagName('p');
+  });
+
+  it('sets class name', () => {
+    const component = render(<ProgressMeterText/>);
+    expect(component).to.have.className('progress-meter-text');
+  });
+
+  it('sets custom class name', () => {
+    const component = render(<ProgressMeterText className="my-progress-meter-text"/>);
+    expect(component).to.have.className('my-progress-meter-text');
+    expect(component).to.not.have.className('progress-meter-text');
+  });
+
+  it('sets contents', () => {
+    const component = render(<ProgressMeterText>25%</ProgressMeterText>);
+    expect(component).to.have.text('25%');
+  });
+  
+});
+
+describe('NativeProgress component', () => {
+
+  it('sets tag name', () => {
+    const component = render(<NativeProgress maxValue={100} currentValue={75}/>);
+    expect(component).to.have.tagName('progress');
+  });
+
+  it('sets custom class name', () => {
+    const component = render(<NativeProgress className="my-progress" maxValue={100} currentValue={75}/>);
+    expect(component).to.have.className('my-progress');
+  });
+
+  it('sets color', () => {
+    const component = render(<NativeProgress color={ProgressColors.SUCCESS} maxValue={100} currentValue={75}/>);
+    expect(component).to.have.className('success');
+    expect(component).to.not.have.attr('color');
+  });
+
+  it('sets maximum value', () => {
+    const component = render(<NativeProgress maxValue={100} currentValue={75}/>);
+    expect(component).to.have.attr('max', '100');
+  });
+
+  it('sets current value', () => {
+    const component = render(<NativeProgress maxValue={100} currentValue={50}/>);
+    expect(component).to.have.attr('value', '50');
+  });
+
+});
