4591acd6dff58afd8e189ada726ef9acd9eed6af
Christoffer Niska <christofferniska@gmail.com>
2016-03-12 16:32:07 +0200

    Add Callout component

---
+++ src/components/callout.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import check from 'check-types';
+import classNames from 'classnames';
+import { default as omit } from 'lodash.omit';
+
+/**
+ * Callout color enumerable.
+ *
+ * @type {Object}
+ */
+export const CalloutColors = {
+  PRIMARY: 'primary',
+  SECONDARY: 'secondary',
+  SUCCESS: 'success',
+  WARNING: 'warning',
+  ALERT: 'alert'
+};
+
+/**
+ * Callout size enumerable.
+ *
+ * @type {Object}
+ */
+export const CalloutSizes = {
+  SMALL: 'small',
+  LARGE: 'large'
+};
+
+const omitProps = [
+  'color',
+  'size'
+];
+
+/**
+ * Callout component.
+ * http://foundation.zurb.com/sites/docs/callout.html
+ *
+ * @param {Object} props
+ * @returns {XML}
+ */
+const Callout = props => {
+  check.assert.maybe.string(props.color, 'Callout.props.color must be a string.');
+  check.assert.maybe.string(props.size, 'Callout.props.size must be a string.');
+
+  const className = classNames(
+    props.className || 'callout',
+    props.color,
+    props.size
+  );
+
+  return (
+    <div {...omit(props, omitProps)} className={className}></div>
+  );
+};
+
+// TODO: Add support for closable elements.
+
+export default Callout;
