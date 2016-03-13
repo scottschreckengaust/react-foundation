dad9b8c960313a62942831050e7de4859f900fbc
Christoffer Niska <christofferniska@gmail.com>
2016-03-13 22:39:48 +0200

    Fix some bugs in the switch components

--- src/components/switch.js
+++ src/components/switch.js
@@ -3,6 +3,8 @@ import check from 'check-types';
 import classNames from 'classnames';
 import { default as omit } from 'lodash.omit';
 
+let currentId = 0;
+
 /**
  * Switch type enumerable.
  *
@@ -33,20 +35,25 @@ export const SwitchSizes = {
  */
 export const Switch = props => {
   check.assert.maybe.string(props.size, 'Switch.props.size must be a string.');
+  check.assert.maybe.string(props.id, 'Switch.props.id must be a string.');
 
   const className = classNames(
     props.className || 'switch',
     props.size
   );
 
-  const omitProps = ['input', 'paddle'];
+  const switchId = props.id || `switch${currentId++}`;
+  const omitProps = ['id', 'size', 'input', 'paddle'];
+
+  // TODO: Consider refactoring this, the rendering a little bit messy...
 
   return (
     <div {...omit(props, omitProps)} className={className}>
-      <SwitchInput {...props.input}/>
-      <SwitchPaddle {...props.paddle}/>
-      {props.active ? <SwitchActive {...props.active}/> : null}
-      {props.inactive ? <SwitchInactive {...props.inactive}/> : null}
+      <SwitchInput {...props.input} id={switchId}/>
+      <SwitchPaddle {...props.paddle} htmlFor={switchId}>
+        {props.active ? <SwitchActive {...props.active}/> : null}
+        {props.inactive ? <SwitchInactive {...props.inactive}/> : null}
+      </SwitchPaddle>
     </div>
   );
 };
@@ -61,7 +68,7 @@ export const SwitchInput = props => {
   check.assert.maybe.string(props.type, 'SwitchInput.props.type must be a string.');
 
   const omitProps = ['type'];
-  
+
   return (
     <input {...omit(props, omitProps)} className={props.className || 'switch-input'} type={props.type || SwitchTypes.CHECKBOX}/>
   );
@@ -84,7 +91,9 @@ export const SwitchPaddle = props => (
  * @returns {XML}
  */
 export const SwitchActive = props => (
-  <span {...props} className={props.className || 'switch-active'} aria-hidden="true"/>
+  <span {...props} className={props.className || 'switch-active'} aria-hidden="true">
+    {props.text}
+  </span>
 );
 
 /**
@@ -94,7 +103,9 @@ export const SwitchActive = props => (
  * @returns {XML}
  */
 export const SwitchInactive = props => (
-  <span {...props} className={props.className || 'switch-inactive'} aria-hidden="true"/>
+  <span {...props} className={props.className || 'switch-inactive'} aria-hidden="true">
+    {props.text}
+  </span>
 );
 
 export default Switch;
--- test/components/switch-spec.js
+++ test/components/switch-spec.js
@@ -24,24 +24,24 @@ describe('Switch component', () => {
 
   it('has correct children', () => {
     const renderer = createRenderer();
-    renderer.render(<Switch/>);
+    renderer.render(<Switch id="foo"/>);
     const output = renderer.getRenderOutput();
-    expect(output).jsx.to.equal(<div className="switch"><SwitchInput/><SwitchPaddle/></div>);
+    expect(output).jsx.to.equal(<div className="switch"><SwitchInput id="foo"/><SwitchPaddle htmlFor="foo"/></div>);
   });
 
   it('passes on props', () => {
-    const component = mount(<Switch input={{ value: 'foo' }} paddle={{ htmlFor: 'bar' }}/>);
+    const component = mount(<Switch input={{ value: 'foo' }} paddle={{ id: 'bar' }}/>);
     expect(component.find('.switch-input')).to.have.value('foo');
-    expect(component.find('.switch-paddle')).to.have.attr('for', 'bar');
+    expect(component.find('.switch-paddle')).to.have.attr('id', 'bar');
   });
 
   it('renders active label', () => {
-    const component = mount(<Switch active={{ children: 'On' }}/>);
+    const component = mount(<Switch active={{ text: 'On' }}/>);
     expect(component.find('.switch-active')).to.have.text('On');
   });
 
   it('renders inactive label', () => {
-    const component = mount(<Switch inactive={{ children: 'Off' }}/>);
+    const component = mount(<Switch inactive={{ text: 'Off' }}/>);
     expect(component.find('.switch-inactive')).to.have.text('Off');
   });
 
@@ -105,6 +105,12 @@ describe('SwitchActive component', () => {
     expect(component).to.not.have.className('switch-active');
   });
 
+  it('sets contents', () => {
+    const component = render(<SwitchActive text="On"/>);
+    expect(component).to.have.text('On');
+    expect(component).to.not.have.attr('text');
+  });
+
 });
 
 describe('SwitchInactive component', () => {
@@ -123,6 +129,12 @@ describe('SwitchInactive component', () => {
     const component = render(<SwitchInactive className="my-switch-inactive"/>);
     expect(component).to.have.className('my-switch-inactive');
     expect(component).to.not.have.className('switch-inactive');
+  });
+
+  it('sets contents', () => {
+    const component = render(<SwitchInactive text="Off"/>);
+    expect(component).to.have.text('Off');
+    expect(component).to.not.have.attr('text');
   });
 
 });
