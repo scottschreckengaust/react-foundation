83e2a7628cfa4595cd78fdcddab990f9de275681
Christoffer Niska <christofferniska@gmail.com>
2017-06-21 22:24:49 +0300

    Change to use prop-types library

--- src/components/callout.js
+++ src/components/callout.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types'
 import { CalloutColors, CalloutSizes } from '../enums';
 import { GeneralPropTypes, createClassName, generalClassNames, removeProps, objectKeys, objectValues } from '../utils';
 
