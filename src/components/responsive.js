cd86362af5add4113e8b896d46e4531539531bca
larpo <larpo@iki.fi>
2017-06-23 23:43:46 +0900

    Convert remaining PropTypes imports to use prop-types library (#34)

--- src/components/responsive.js
+++ src/components/responsive.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { TopBar } from './top-bar';
 import { GeneralPropTypes, createClassName, generalClassNames, removeProps } from '../utils';
 
